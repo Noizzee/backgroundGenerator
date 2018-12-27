edc0b6e26d03cfc0ea940b06c55cd4d0820eada2
Noizzee <37045144+Noizzee@users.noreply.github.com>
2018-12-27 13:01:07 +0100

    added randomize button and code

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.getElementById("gradient");
+var randomBtn = document.querySelector("#random");
 
 
 
@@ -17,6 +18,16 @@ function setColor() {
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
   css.textContent = body.style.background + ";";
 }
+
+function randomize(random1, random2, random3) {
+  random1 = Math.floor(Math.random() * 255);
+  random2 = Math.floor(Math.random() * 255);
+  random3 = Math.floor(Math.random() * 255);
+  body.style.background = "linear-gradient(to right, rgb(" + random1 + ", " + random2 + ", " + random3 + "), rgb(" + random2 + ", " + random3 + ", " + random1 + ")";
+  css.textContent = body.style.background + ";";
+}
+
+randomBtn.addEventListener("click", randomize);
 
 color1.addEventListener("input", changeBackground);
 
