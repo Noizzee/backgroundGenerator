1716e2a5d1fc09a994841e3695c29b180a6ca02e
Noizzee <37045144+Noizzee@users.noreply.github.com>
2018-12-27 12:12:54 +0100

    added onload event

--- js/script.js
+++ js/script.js
@@ -4,7 +4,16 @@ var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.getElementById("gradient");
 
+
+
 function changeBackground() {
+  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = body.style.background + ";";
+}
+
+function setColor() {
+  color1.value = '#ff0000';
+  color2.value = '#ffff00';
   body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
   css.textContent = body.style.background + ";";
 }
@@ -12,3 +21,5 @@ function changeBackground() {
 color1.addEventListener("input", changeBackground);
 
 color2.addEventListener("input", changeBackground);
+
+window.addEventListener("load", setColor);
