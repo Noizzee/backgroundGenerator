231b99bdd894f394d4d150c5378fd30de10b56d1
Noizzee <37045144+Noizzee@users.noreply.github.com>
2019-01-06 11:30:55 +0100

    changed to ES6 syntax, added value on randomize

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
 //Caching the DOM
-var css = document.querySelector("h3");
-var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
-var body = document.getElementById("gradient");
-var randomBtn = document.querySelector("#random");
+const css = document.querySelector("h3");
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const body = document.getElementById("gradient");
+const randomBtn = document.querySelector("#random");
 
 
 
 function changeBackground() {
-  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   css.textContent = body.style.background + ";";
 }
 
 function setColor() {
   color1.value = '#ff0000';
   color2.value = '#ffff00';
-  body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
   css.textContent = body.style.background + ";";
 }
 
 function randomize() {
-  var randomNumbers = [];
-  for (let i = 0; i < 6; i++) {
-    randomNumbers.push(Math.floor(Math.random() * 255));
-  }
-  body.style.background = "linear-gradient(to right, rgb(" + randomNumbers[0] + ", " + randomNumbers[1] + ", " + randomNumbers[2] + "), rgb(" + randomNumbers[3] + ", " + randomNumbers[4] + ", " + randomNumbers[5] + ")";
+  let randomColor1 = Math.floor(Math.random() * 16777215).toString(16);
+  let randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
+  body.style.background = `linear-gradient(to right, #${randomColor1}, #${randomColor2})`;
   css.textContent = body.style.background + ";";
+  color1.value = `#${randomColor1}`;
+  color2.value = `#${randomColor2}`;
 }
 
 randomBtn.addEventListener("click", randomize);
